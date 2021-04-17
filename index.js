51acc9b1c9375cc980f8dd1b827fa59aa03fd342
Patrick <zhen.patrick@gmail.com>
2021-04-17 14:55:28 -0400

    changed port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use("/", superheroController)
 
-app.set("port", process.env.PORT || 4000);
+// app.set("port", process.env.PORT || 4000);
 
 // app.listen(app.get('port'), () =>{
 //     console.log(`connected on PORT: ${app.get('port')}`)
