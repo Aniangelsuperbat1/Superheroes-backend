57a0051af83f7d008a4277352611be563f413709
Patrick <zhen.patrick@gmail.com>
2021-04-18 18:34:32 -0400

    added start script

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(cors());
 
 app.use("/", superheroController)
 
-// app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
 
-// app.listen(app.get('port'), () =>{
-//     console.log(`connected on PORT: ${app.get('port')}`)
-// })
+app.listen(app.get('port'), () =>{
+    console.log(`connected on PORT: ${app.get('port')}`)
+})
 
-app.listen(process.env.PORT || 4000)
+// app.listen(process.env.PORT || 4000)
+
 
