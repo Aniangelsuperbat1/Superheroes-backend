7003b10f738e87eb35a81fee674d463804162a31
Patrick <zhen.patrick@gmail.com>
2021-04-26 19:42:35 -0400

    added more routes

--- controllers/superhero.js
+++ controllers/superhero.js
@@ -8,9 +8,27 @@ router.get("/", (req, res, next) =>{
     }).catch(next)
 })
 
+router.get("/DC/heroes", (req, res, next) => {
+  superhero
+    .find({status: "hero", universe: "DC"})
+    .then((hero) => {
+      res.json(hero);
+    })
+    .catch(next);
+});
+
 router.get("/DC/villains", (req, res, next) => {
   superhero
-    .find({status: "Villain"})
+    .find({status: "Villain", universe: "DC"})
+    .then((hero) => {
+      res.json(hero);
+    })
+    .catch(next);
+});
+
+router.get("/Marvel/heroes", (req, res, next) => {
+  superhero
+    .find({ status: "hero", universe: "Marvel" })
     .then((hero) => {
       res.json(hero);
     })
