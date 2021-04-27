75cf8ec06aadd808f0e58d54a279f96c78d17fcd
Patrick <zhen.patrick@gmail.com>
2021-04-26 20:36:39 -0400

    fixing finilizing of database

--- controllers/superhero.js
+++ controllers/superhero.js
@@ -3,7 +3,7 @@ const superhero = require("../Models/Superhero")
 const router = express.Router();
 
 router.get("/", (req, res, next) =>{
-    superhero.find({}).then((hero) => {
+    superhero.find({ status: "Hero" }).then((hero) => {
         res.json(hero)
     }).catch(next)
 })
